abe843dbd079155718be46118acf8317724e9f2f
camelCase <153457928+2000akansha@users.noreply.github.com>
2024-07-02 14:57:46 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,46 +7,53 @@ export const Context = createContext({
   isAuthorized: false,
   user: {},
   token: "",
+  role: "",
   setIsAuthorized: () => {},
   setUser: () => {},
   setToken: () => {},
-  
+  setRole: () => {},
 });
 
-// Create a component to wrap the entire app with the context provider
 export const AppWrapper = () => {
-  // State variables for authorization, user data, and token
   const [isAuthorized, setIsAuthorized] = useState(false);
   const [user, setUser] = useState({});
   const [token, setToken] = useState("");
+  const [role, setRole] = useState("");
 
-  // Function to update the token
   const handleSetToken = (newToken) => {
     setToken(newToken);
   };
 
+  const handleSetUser = (userData) => {
+    console.log("Setting user data:", userData); // Log user data
+    setUser(userData);
+    if (userData.role) {
+      setRole(userData.role);
+    } else {
+      console.log("Role is missing in user data");
+    }
+  };
+
   return (
-    // Provide the context values to the entire app
     <Context.Provider
       value={{
         isAuthorized,
         setIsAuthorized,
         user,
-        setUser,
+        setUser: handleSetUser,
         token,
         setToken: handleSetToken,
+        role,
+        setRole,
       }}
     >
-      {/* Render the main app component */}
       <App />
     </Context.Provider>
   );
 };
 
-// Use ReactDOM.createRoot instead of ReactDOM.render
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* Wrap the app with the AppWrapper component */}
     <AppWrapper />
   </React.StrictMode>
 );
